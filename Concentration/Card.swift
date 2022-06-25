e1d3bf79252fe5c837c459752606c6cb4a212192
qlskv <htkv582@naver.com>
2022-06-25 16:52:41 +0900

    inital

---
+++ Concentration/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by 이순빈 on 2021/06/25.
+//
+
+import Foundation
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,8 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by 이순빈 on 2021/06/25.
+//
+
+import Foundation
